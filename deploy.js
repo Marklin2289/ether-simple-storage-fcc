f4cfc358e0018198e8620ee8708e69c6df4d79ad
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2022-06-13 22:24:35 -0700

    cleaning up

--- deploy.js
+++ deploy.js
@@ -1,17 +1,13 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
+require("dotenv").config();
 
 async function main() {
   //   compile them in our code
   // compile them separately
   //   http://127.0.0.1:7545
-  const provider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:7545"
-  );
-  const wallet = new ethers.Wallet(
-    "e9becad8068b72183985c7bb585e54f1ba16de702e80d505ac28009a85eb562b",
-    provider
-  );
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
